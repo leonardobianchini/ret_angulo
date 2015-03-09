5bedd75ee372cb3c0abef870004605e78e7b4462
LeonardoBianchini <leonardobianchini7@gmail.com>
2015-03-08 22:28:22 -0300

    implementacao dos atributos e das operacoes no arquivo Retangulo.java

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 class Main{
-	public static void main(string args[]){
-		
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int n=1;
+		while (n==1){
+			Retangulo ret = new Retangulo();
+			System.out.println("Digite a base do retangulo");
+			ret.base = scan.nextInt();
+			System.out.println("Digite a altura do retangulo");
+			ret.altura = scan.nextInt();
+			ret.calc();
+			System.out.println("Novamente digite 1 \nSair digite 0");
+			n = scan.nextInt();
+		}
 	}
 }
--- Retangulo.java
+++ Retangulo.java
@@ -1,11 +1,11 @@
 class Retangulo{
-	public double base;
-	public double altura;
-	public void area_peri(){
-		double result;
+	public int base;
+	public int altura;
+	public void calc(){
+		int result;
 		result = (2*base)+(2*altura);
 		System.out.println("Perimetro = "+result);
 		result = (base*altura);
-		System.out.println("Area = "+result);
+		System.out.println("Area = "+result+"\n");
 	}
 }
