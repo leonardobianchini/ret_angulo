f4a08ab7cea52752fea52a1cc1e55f8dfef6af16
LeonardoBianchini <leonardobianchini7@gmail.com>
2015-03-08 21:08:49 -0300

    implementacao dos atributos e das operacoes no arquivo Retangulo.java

---
+++ Retangulo.java
@@ -0,0 +1,11 @@
+class Retangulo{
+	public double base;
+	public double altura;
+	public void area_peri(){
+		double result;
+		result = (2*base)+(2*altura);
+		System.out.println("Perimetro = "+result);
+		result = (base*altura);
+		System.out.println("Area = "+result);
+	}
+}
